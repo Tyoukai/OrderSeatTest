6b71224f190f4a0751426c6a460c9ff040e734f5
张珂为 <zhang_k_w@163.com>
2019-12-25 13:19:00 +0800

    占座客户端

--- src/main/java/com/orderseat/test/OrderSeatClient.java
+++ src/main/java/com/orderseat/test/OrderSeatClient.java
@@ -1,54 +1,30 @@
 package com.orderseat.test;
 
+import com.orderseat.facade.OrderSeatService;
 import com.orderseat.facade.SeatInfoService;
-import com.orderseat.facade.dto.SeatInfoDto;
-import com.orderseat.facade.request.SeatInfoRequest;
-import com.orderseat.facade.response.SeatInfoResponse;
+import com.orderseat.facade.dto.OrderSeatDto;
+import com.orderseat.facade.request.OrderSeatRequest;
 import org.apache.thrift.TException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.ArrayList;
 import java.util.List;
 
-/**
- * @author: tyoukai
- * @date: 2019-12-14 19:37
- * @description: 占座测试类
- * @version: v1.0
- */
 public class OrderSeatClient {
     public static void main(String[] args) throws TException {
-        SeatInfoDto seatInfoDto = new SeatInfoDto();
-        // 座位id
-        seatInfoDto.setId("1");
-        // 建筑id
-        seatInfoDto.setBuildingId("1");
-        // 楼层
-        seatInfoDto.setFloorId("1");
-        // 座位类型
-        seatInfoDto.setSeatType("");
-
-        SeatInfoRequest seatInfoRequest = new SeatInfoRequest();
-        seatInfoRequest.setSeatInfoDto(seatInfoDto);
-        // 座位开始位置
-        seatInfoRequest.setPageNum(1);
-        // 总共返回的条数
-        seatInfoRequest.setPageSize(10);
-
+        OrderSeatDto orderSeatDto = new OrderSeatDto();
+        orderSeatDto.setId("1");
+        orderSeatDto.setStartTime("201912240800");
+        orderSeatDto.setEndTime("201912241000");
+        orderSeatDto.setUserId("0");
+        List<OrderSeatDto> list = new ArrayList<OrderSeatDto>();
+        list.add(orderSeatDto);
+        OrderSeatRequest orderSeatRequest = new OrderSeatRequest();
+        orderSeatRequest.setOrderSeatDtoList(list);
         BeanFactory beanFactory = new ClassPathXmlApplicationContext("spring-config.xml");
 
-        SeatInfoService.Iface client = (SeatInfoService.Iface) beanFactory.getBean("SeatInfoClient");
-
-        // 查询到的座位信息
-        SeatInfoResponse seatInfoResponse = client.search(seatInfoRequest);
-
-        List<SeatInfoDto> list = seatInfoResponse.getSeatInfoDtoList();
-
-        for (SeatInfoDto seatInfoDto1 : list) {
-            System.out.println("座位id：" + seatInfoDto1.getId());
-        }
-        System.out.println("座位状态：" + seatInfoResponse.getResultCode());
-        System.out.println("座位描述：" + seatInfoResponse.getDesc());
-
+        OrderSeatService.Iface client = (OrderSeatService.Iface) beanFactory.getBean("OrderSeatClient");
+        client.orderSeat(orderSeatRequest);
     }
 }
